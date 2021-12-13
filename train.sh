19e070ac5767bc3dbab10ec913b5ac8aa5f60059
natethegreat4 <natealias@gmail.com>
2021-12-13 17:51:59 -0600

    Showing full prefix in echo

--- train.sh
+++ train.sh
@@ -4,7 +4,7 @@ prefix=$3
 
 for num_agents in $(seq 2 $max_agents); do
   for num_landmarks in $(seq 2 $max_landmarks); do
-    echo "python train.py --max_agents $num_agents --min_agents $num_agents --min_landmarks $num_landmarks --max_landmarks $num_landmarks --prefix $prefix"
+    echo "python train.py --max_agents $num_agents --min_agents $num_agents --min_landmarks $num_landmarks --max_landmarks $num_landmarks --prefix $prefix/agents_$num_agents/landmarks_$num_landmarks/"
     python train.py --max-agents $num_agents --min-agents $num_agents --min-landmarks $num_landmarks --max-landmarks $num_landmarks --prefix $prefix/agents_$num_agents/landmarks_$num_landmarks/
   done
 done
