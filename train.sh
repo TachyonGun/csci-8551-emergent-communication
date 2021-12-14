2268a5d795c2a7dc817955e6271401388121ae3d
natethegreat4 <natealias@gmail.com>
2021-12-13 18:52:16 -0600

    Updating training script to use 50 epochs

--- train.sh
+++ train.sh
@@ -6,6 +6,6 @@ for num_agents in $(seq 2 $max_agents); do
   for num_landmarks in $(seq 2 $max_landmarks); do
     echo "python train.py --max_agents $num_agents --min_agents $num_agents --min_landmarks $num_landmarks --max_landmarks $num_landmarks --prefix $prefix/agents_$num_agents/landmarks_$num_landmarks/"
     mkdir -p $prefix/agents_$num_agents/landmarks_$num_landmarks/
-    python train.py --max-agents $num_agents --min-agents $num_agents --min-landmarks $num_landmarks --max-landmarks $num_landmarks --prefix $prefix/agents_$num_agents/landmarks_$num_landmarks/
+    python train.py --max-agents $num_agents --min-agents $num_agents --min-landmarks $num_landmarks --max-landmarks $num_landmarks --prefix $prefix/agents_$num_agents/landmarks_$num_landmarks/ --epochs 50
   done
 done
